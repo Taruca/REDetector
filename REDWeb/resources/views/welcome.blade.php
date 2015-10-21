ece37698914b1f6f5c547b0452953f80de1604df
LIU <liuziyang_lzy@163.com>
2015-10-21 16:31:56 +0800

    change the title of welcome page to test github

--- REDWeb/resources/views/welcome.blade.php
+++ REDWeb/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">REDetector</div>
             </div>
         </div>
     </body>
